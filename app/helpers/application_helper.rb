c029c98894a9ff8f9324dfef1f3097ccf2da54c7
pkrip <p.criparacos@gmail.com>
2024-02-14 18:02:04 +0200

    Configured helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+#require 'navbar_helper.rb'
+
 module ApplicationHelper
+    include NavbarHelper
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Project01
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_controller.include_all_helpers = false
   end
 end
